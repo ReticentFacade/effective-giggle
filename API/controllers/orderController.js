2a2702b250793baba429c82ab4f978832dc1f1aa
Reticent Facade <Reticent@mac.local>
2023-07-05 11:26:30 +0530

    order

--- API/controllers/orderController.js
+++ API/controllers/orderController.js
@@ -34,9 +34,38 @@ const getOrder = async (req, res) => {
   }
 };
 
+const deleteOrder = async(req, res) => {
+  try {
+    const { orderId } = req.params;
+
+    // Delete from database using Order-model:
+    let deletedOrder = Order.findByPk(req.params.id);
+    Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((deletedOrder) => {
+        res.json("Removed order from database")
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+    // const order = await Order.destroy({
+    //   where: { orderId:  },
+    // });
+
+    res.status(200).json({ message: "Order deleted successfully!", order });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Unable to delete the order!" });
+  }
+};
+
 const orderController = {
     placeOrder,
     getOrder,
+    deleteOrder,
 };
 
 export default orderController;
--- API/routes/handler/order-routes.js
+++ API/routes/handler/order-routes.js
@@ -1,14 +1,14 @@
 import Router from "express";
-import orderController from "../../controllers/order-controller.js";
+import orderController from "../../controllers/orderController.js";
 
 const orderRouter = new Router();
 
-orderRouter.get("/orders", orderController.getOrders);
-orderRouter.post("/orders", orderController.createOrder);
+orderRouter.get("/orders", orderController.getOrder);
+orderRouter.post("/orders", orderController.placeOrder);
 // -----
 // One controller to send it to the seller??
 // -----
-orderRouter.put("/orders/:id", orderController.updateOrder);
+// orderRouter.put("/orders/:id", orderController.updateOrder);
 orderRouter.delete("/orders/:id", orderController.deleteOrder);
 
 export default orderRouter;
